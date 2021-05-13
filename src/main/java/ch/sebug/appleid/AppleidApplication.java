1c39d72855909a3b5d4aafc75fa719c68dfdf411
sebug <sebastian.gfeller@gmail.com>
2021-05-13 12:31:44 +0200

    Preparing for apple ID

---
+++ src/main/java/ch/sebug/appleid/AppleidApplication.java
@@ -0,0 +1,13 @@
+package ch.sebug.appleid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/sebug/appleid/AppleidApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.sebug.appleid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
